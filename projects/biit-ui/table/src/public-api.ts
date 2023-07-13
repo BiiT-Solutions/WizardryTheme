9d4796ce6f88aa5092445e18959a43a47ea17142
Krystal Benlloch <krystal.benlloch@maistering.com>
2023-07-13 09:45:57 +0200

    Updated Biit Table public-api exports

--- projects/biit-ui/table/src/public-api.ts
+++ projects/biit-ui/table/src/public-api.ts
@@ -4,4 +4,13 @@
 
 export * from './biit-table/biit-table.component';
 export * from './biit-table/biit-table.module';
-export * from './biit-table/basic-table-data';
+export * from './biit-table/models/biit-table-data';
+export * from './biit-table/models/biit-table-column';
+export * from './biit-table/models/biit-table-response';
+export * from './biit-table/models/biit-table-sorting';
+export * from './biit-table/directives/biit-table-sortable.directive';
+export * from './biit-table/directives/biit-table-selectable.directive';
+export * from './biit-paginator/biit-paginator.component';
+export * from './biit-paginator/biit-paginator.module';
+export * from './biit-paginator/models/biit-paginator-options';
+
