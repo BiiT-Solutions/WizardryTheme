4bc771fb9879b804ff8330fe8de5c6c42f38a550
Krystal Benlloch <krystal.benlloch@maistering.com>
2023-07-25 11:41:54 +0200

    Updated biit-button selector on stories

--- src/stories/basic/biit-tooltip.stories.ts
+++ src/stories/basic/biit-tooltip.stories.ts
@@ -18,42 +18,42 @@ const Template: Story<BiitButtonComponent> = (args: BiitButtonComponent) => ({
         style="position:fixed; top:25%; left:50%; translate: -50% -50%">
       Hover over buttons to show tooltips
     </h3>
-    <biit-button tooltip="Noticias breves, ya está aquí para ustedes Mr. Quitanieves"
+    <button biit-button tooltip="Noticias breves, ya está aquí para ustedes Mr. Quitanieves"
                  style="position: fixed; top: 1rem; left: 1rem;">
       Lawful good
-    </biit-button>
-    <biit-button tooltip="Do it for her"
+    </button>
+    <button biit-button tooltip="Do it for her"
                  style="position: fixed; top: 1rem; left: 50%; translate: -50% 0;">
       Neutral good
-    </biit-button>
-    <biit-button tooltip="¡Con la de hierro que tiene!"
+    </button>
+    <button biit-button tooltip="¡Con la de hierro que tiene!"
                  style="position: fixed; top: 1rem; right: 1rem;">
       Chaotic good
-    </biit-button>
-    <biit-button tooltip="Es viernes de siluetas"
+    </button>
+    <button biit-button tooltip="Es viernes de siluetas"
                  style="position: fixed; top: 50%; left: 1rem; translate: 0 -50%;">
       Lawful neutral
-    </biit-button>
-    <biit-button tooltip="El dinero se puede intercambiar por bienes y servicios"
+    </button>
+    <button biit-button tooltip="El dinero se puede intercambiar por bienes y servicios"
                  style="position: fixed; top: 50%; left: 50%; translate: -50% -50%;">
       True neutral
-    </biit-button>
-    <biit-button tooltip="Este tooltip es una divinidad, ¿POR QUÉ EL MÍO NO ES IGUAL?"
+    </button>
+    <button biit-button tooltip="Este tooltip es una divinidad, ¿POR QUÉ EL MÍO NO ES IGUAL?"
                  style="position: fixed; top: 50%; right: 1rem; translate: 0 -50%;">
       Chaotic neutral
-    </biit-button>
-    <biit-button tooltip="-Debe responder con sinceridad, ¿lo entiende? +Sí. *Explota*"
+    </button>
+    <button biit-button tooltip="-Debe responder con sinceridad, ¿lo entiende? +Sí. *Explota*"
                  style="position: fixed; bottom: 1rem; left: 1rem;">
       Lawful evil
-    </biit-button>
-    <biit-button tooltip="¿La aurora boreal? ¿En esta época del año, a esta hora del día, en esta zona del país, localizada exclusivamente en su cocina?"
+    </button>
+    <button biit-button tooltip="¿La aurora boreal? ¿En esta época del año, a esta hora del día, en esta zona del país, localizada exclusivamente en su cocina?"
                  style="position: fixed; bottom: 1rem; left: 50%; translate: -50% 0;">
       Neutral evil
-    </biit-button>
-    <biit-button tooltip="Sin tele y sin cerveza, Homer pierde la cabeza"
+    </button>
+    <button biit-button tooltip="Sin tele y sin cerveza, Homer pierde la cabeza"
                  style="position: fixed; bottom: 1rem; right: 1rem; translate: 0 0;">
       Chaotic evil
-    </biit-button>
+    </button>
 `});
 
 export const Default = Template.bind({});
