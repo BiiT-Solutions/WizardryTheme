34e99556adce79195ef69630aaa30a9d2f057286
kbenlloch <kbenlloch@biit-solutions.com>
2024-02-15 14:58:25 +0100

    Updated Biit Tab Group display parameters for flexibility

--- projects/biit-ui/navigation/src/biit-tab-group/biit-tab.component.ts
+++ projects/biit-ui/navigation/src/biit-tab-group/biit-tab.component.ts
@@ -2,8 +2,8 @@ import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'biit-tab',
-  template: '<div [hidden]="!active" style="display: contents; min-height: 100%"><ng-content></ng-content></div>',
-  styleUrls: []
+  template: '<div [hidden]="!active" [class.inner-content]="active"><ng-content></ng-content></div>',
+  styleUrls: ['biit-tab.component.scss']
 })
 
 export class BiitTabComponent {
