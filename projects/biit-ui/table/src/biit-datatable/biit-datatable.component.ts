2a01e07fb47b3ec374ba264adeb03b6adbb7b522
kbenlloch <kbenlloch@biit-solutions.com>
2024-08-13 13:48:19 +0200

    fix: biit-datatable visible columns

--- projects/biit-ui/table/src/biit-datatable/biit-datatable.component.ts
+++ projects/biit-ui/table/src/biit-datatable/biit-datatable.component.ts
@@ -97,7 +97,7 @@ export class BiitDatatableComponent<T> implements OnInit {
   }
 
   setColumnVisibility(columns: DatatableColumn[]) {
-    this._columns = this.allColumns.filter(c => columns.find(col => c.$$id == col.$$id))
+    this._columns = this.allColumns.filter(c => columns.includes(c))
   }
 
   log(...event) {
