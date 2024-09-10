f771c50a268c1c98086396115778037cd2178625
kbenlloch <kbenlloch@biit-solutions.com>
2024-09-10 16:48:53 +0200

    added: context menu component styling

---
+++ src/stories/navigation/context-menu.stories.ts
@@ -0,0 +1,52 @@
+import { Story, Meta, moduleMetadata } from '@storybook/angular';
+import {BiitNavUserComponent} from 'biit-ui/navigation';
+import {ContextMenuModule} from "@perfectmemory/ngx-contextmenu";
+import {BiitButtonModule} from "biit-ui/button";
+import {BiitIconModule, BiitIconService} from "biit-ui/icon";
+import {completeIconSet} from "biit-icons-collection";
+import {APP_INITIALIZER} from "@angular/core";
+
+function biitIconServiceFactory(service: BiitIconService) {
+  service.registerIcons(completeIconSet);
+  return () => service;
+}
+
+export default {
+  title: 'Navigation/Context Menu',
+  decorators: [
+    moduleMetadata({
+      imports: [ContextMenuModule, BiitButtonModule, BiitIconModule],
+      providers: [{
+        provide: APP_INITIALIZER,
+        useFactory: biitIconServiceFactory,
+        multi: true,
+        deps: [BiitIconService],
+      }]
+    }),
+  ],
+  args: {
+    title: 'John Doe',
+    subtitle: ''
+  }
+} as Meta;
+
+const Template: Story<BiitNavUserComponent> = (args: BiitNavUserComponent) => ({
+  props: args,
+  template:`
+    <button biit-button [contextMenu]="contextMenu">Show</button>
+    <context-menu #contextMenu [menuClass]="biit-contextmenu">
+        <ng-template contextMenuItem><biit-icon name="cauldron"/>Test 1</ng-template>
+        <ng-template contextMenuItem>Testing 2</ng-template>
+        <ng-template contextMenuItem><biit-icon name="column_selection"/>Testing even longer 3</ng-template>
+        <ng-template contextMenuItem>Testing a much much longer title because reasons</ng-template>
+        <ng-template contextMenuItem [divider]="true"></ng-template>
+        <ng-template contextMenuItem [subMenu]="subMenu">Sub-menu</ng-template>
+        <context-menu #subMenu>
+            <ng-template contextMenuItem>Without icon</ng-template>
+            <ng-template contextMenuItem><biit-icon name="cross"/>With icon</ng-template>
+        </context-menu>
+    </context-menu>
+`
+});
+
+export const Default = Template.bind({});
