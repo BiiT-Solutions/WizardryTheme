5f4ff8fdb9e7a3beb6a06c5cd5a07dc2ae546df4
kbenlloch <kbenlloch@biit-solutions.com>
2024-09-13 13:16:11 +0200

    added: error handler utility

---
+++ projects/biit-ui/utils/src/error-handler/error-handler.ts
@@ -0,0 +1,17 @@
+import {TranslocoService} from "@ngneat/transloco";
+import {BiitSnackbarService, NotificationType} from "biit-ui/info";
+import {HttpErrorResponse} from "@angular/common/http";
+
+export class ErrorHandler {
+  public static notify(error: HttpErrorResponse, translocoService: TranslocoService, snackbarService: BiitSnackbarService) {
+    if (error.error.code) {
+      translocoService.selectTranslate(error.error.code, {}, {scope:'biit-ui/utils'}).subscribe(msg => {
+        snackbarService.showNotification(msg, NotificationType.ERROR, undefined, 10);
+      });
+      return;
+    }
+    translocoService.selectTranslate('request_unsuccessful', {}, {scope:'biit-ui/utils'}).subscribe(msg => {
+      snackbarService.showNotification(msg, NotificationType.ERROR, null, 5);
+    });
+  }
+}
