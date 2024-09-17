608b913baee098efaa1173fed6d661e941587ea3
kbenlloch <kbenlloch@biit-solutions.com>
2024-09-17 13:54:43 +0200

    added: biit-nav-menu submenu changed to dropdown context menus

--- projects/biit-ui/navigation/src/biit-nav-menu/biit-nav-menu.component.ts
+++ projects/biit-ui/navigation/src/biit-nav-menu/biit-nav-menu.component.ts
@@ -1,5 +1,6 @@
-import {Component, Input} from '@angular/core';
-import {Route} from '@angular/router';
+import {Component, ElementRef, Input, ViewChild} from '@angular/core';
+import {Route, Router} from '@angular/router';
+import {ContextMenuComponent, ContextMenuService} from "@perfectmemory/ngx-contextmenu";
 
 @Component({
   selector: 'biit-nav-menu',
@@ -16,15 +17,38 @@ export class BiitNavMenuComponent {
 
   protected readonly clearTimeout = clearTimeout;
 
-  onMouseMove(route: Route) {
-    if (this.timeout) {
-      clearTimeout(this.timeout);
-    }
-    this.timeout = setTimeout(() => {
-      if (!this.submenuHover) {
-        this.hovered = route;
+  @ViewChild('navbarMenu') contextMenu: ContextMenuComponent<Route>;
+  @ViewChild('navbarMenu', {read: ElementRef}) navbarRef: ElementRef;
+  contextMenuItems = [];
+
+  constructor(protected router: Router,
+              protected contextMenuService: ContextMenuService<Route>) {
+  }
+
+  onContextMenu(route: Route, navItem: HTMLDivElement) {
+    this.hovered = route;
+    this.contextMenuItems = route.children?.map(item => {
+      return {
+        title: item.title,
+        disabled: item.data?.['disabled'],
+        visible: !item.data?.['hidden'],
+        path: item.path,
+        parent: route.path
       }
-    }, 200);
+    })
+    this.timeout = setTimeout(() => {
+        this.openMenu(navItem);
+    }, 50);
+  }
+
+  protected openMenu(navItem: HTMLDivElement): void {
+    this.contextMenuService.show(
+      this.contextMenu,
+      {
+        x: navItem.offsetLeft,
+        y: navItem.offsetHeight
+      }
+    );
   }
 
   log(event) {
