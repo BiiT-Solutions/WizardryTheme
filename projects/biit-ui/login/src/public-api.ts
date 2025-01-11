f3777439ff4b4bb21a4ceee5f9c54d6929104538
Jesus Pastor <jnavalon@biit-solutions.com>
2025-01-11 11:24:15 +0100

    export biit-login-service-support

--- projects/biit-ui/login/src/public-api.ts
+++ projects/biit-ui/login/src/public-api.ts
@@ -5,4 +5,5 @@
 export * from './biit-login/biit-login.component';
 export * from './biit-login/biit-login.module';
 export * from './biit-login/models/sign-up-request';
+export * from './biit-login/models/biit-login-service-support';
 
