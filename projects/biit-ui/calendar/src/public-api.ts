da47ef53c84c5d1c920433ac3568848c269248a2
Jesus Navalon <jesusnavalon@rielec.com>
2025-03-06 17:24:53 +0100

    feat: published calendar-utility

--- projects/biit-ui/calendar/src/public-api.ts
+++ projects/biit-ui/calendar/src/public-api.ts
@@ -7,3 +7,4 @@ export * from './biit-calendar/biit-calendar.module';
 export * from './biit-calendar/models/calendar-event';
 export * from './biit-calendar/models/calendar-event-click-event';
 export * from './utils/event-color';
+export * from './biit-calendar/calendar-utility'
