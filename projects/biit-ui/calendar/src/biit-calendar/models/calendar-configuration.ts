5fc8cd728561ecddda18f67caf6e591e552fee1a
Jesus Pastor <jnavalon@biit-solutions.com>
2025-04-19 20:00:59 +0200

    restored default values

--- projects/biit-ui/calendar/src/biit-calendar/models/calendar-configuration.ts
+++ projects/biit-ui/calendar/src/biit-calendar/models/calendar-configuration.ts
@@ -1,8 +1,8 @@
 export class CalendarConfiguration {
-  createOnDrag: boolean = true;
-  barred: boolean = true;
+  createOnDrag: boolean = false;
+  barred: boolean = false;
 
-  constructor(createdOnDrag: boolean = true, barred: boolean = true) {
+  constructor(createdOnDrag: boolean = false, barred: boolean = false) {
     this.createOnDrag = createdOnDrag;
     this.barred = barred;
   }
