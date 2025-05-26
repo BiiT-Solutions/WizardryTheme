9b54d2b806f522e5a1a5db25fc1b3450ed1ce5c6
Jorge Hortelano <jhortelano@biit-solutions.com>
2025-05-26 18:05:02 +0200

    Restored time

--- projects/biit-ui/utils/src/error-handler.ts
+++ projects/biit-ui/utils/src/error-handler.ts
@@ -6,12 +6,12 @@ export class ErrorHandler {
   public static notify(error: HttpErrorResponse, translocoService: TranslocoService, snackbarService: BiitSnackbarService) {
     if (error?.error?.code) {
       translocoService.selectTranslate(error.error.code, {}, {scope:'biit-ui/utils'}).subscribe(msg => {
-        snackbarService.showNotification(msg, NotificationType.ERROR, undefined, 100);
+        snackbarService.showNotification(msg, NotificationType.ERROR, undefined, 10);
       });
       return;
     }
     translocoService.selectTranslate('request_unsuccessful', {}, {scope:'biit-ui/utils'}).subscribe(msg => {
-      snackbarService.showNotification(msg, NotificationType.ERROR, null, 50);
+      snackbarService.showNotification(msg, NotificationType.ERROR, null, 5);
     });
   }
 }
