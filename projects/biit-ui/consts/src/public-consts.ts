f830fb76ec11cdbe9477bc48e9ea670262727870
Jorge Hortelano <jhortelano@biit-solutions.com>
2025-07-02 12:52:09 +0200

    Adding consts project

---
+++ projects/biit-ui/consts/src/public-consts.ts
@@ -0,0 +1,5 @@
+
+export const MAX_BIG_FIELD_LENGTH: number = 255;
+export const MAX_NORMAL_FIELD_LENGTH: number = 50;
+export const MAX_SMALL_FIELD_LENGTH: number = 20;
+export const MIN_FIELD_LENGTH: number = 3;
