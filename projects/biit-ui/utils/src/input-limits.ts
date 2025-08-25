3f52dc2c799a6f28b381878bfdbdeacd5401801c
Jorge Hortelano <jhortelano@biit-solutions.com>
2025-08-25 10:54:38 +0200

    Added tiny field length

--- projects/biit-ui/utils/src/input-limits.ts
+++ projects/biit-ui/utils/src/input-limits.ts
@@ -2,6 +2,7 @@ export class InputLimits {
   public static readonly MAX_BIG_FIELD_LENGTH: number = 255;
   public static readonly MAX_NORMAL_FIELD_LENGTH: number = 50;
   public static readonly MAX_SMALL_FIELD_LENGTH: number = 25;
+  public static readonly MAX_TINY_FIELD_LENGTH: number = 10;
   public static readonly MIN_FIELD_LENGTH: number = 2;
 }
 
